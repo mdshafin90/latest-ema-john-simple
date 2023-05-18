1d87cdaca201689673c7b71c7f8b43eba165d3c0
shafin bisswas <mdnazmussakibshafin@gmail.com>
2023-05-19 00:35:51 +0600

    log in and sign up form completed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import './Login.css'
+import { Link } from 'react-router-dom';
+import img from '../../../public/google.png'
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login coming sooon</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <div className='form-parent'>
+                <form>
+                    <div className='form-control'>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name='email' placeholder='Your Email' required />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name='password' placeholder='Your Password' required />
+                    </div>
+                    <input className='btn-submit' type="submit" value="Login" />
+                </form>
+            </div>
+            <p className='signup-new'>New To Ema-John ? <small><Link to="/signup">Create New Account</Link></small></p>
+            <hr />
+            <button className='google-login'><img className='google-logo' src={img} alt="" /> Continue With Google</button>
         </div>
     );
 };
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './Signup.css'
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
     return (
-        <div>
-            <h1>This is sign up page</h1>
+        <div className='form-container'>
+            <h2 className='form-title'>Sign Up</h2>
+            <div className='form-parent'>
+                <form>
+                    <div className='form-control'>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name='email' placeholder='Your Email' required />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor="password">Create Password</label>
+                        <input type="password" name='password' placeholder='Create Password' required />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor="confirm">Confirm Password</label>
+                        <input type="password" name='password' placeholder='Confirm Password' required />
+                    </div>
+                    <input className='btn-submit' type="submit" value="Sign Up" />
+                </form>
+            </div>
+            <p className='signup-new'>Already have an Account ? <small><Link to="/login">Please Login</Link></small></p>
         </div>
     );
 };
